50e37636d313239d6e8743674922fdd7234bd302
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2021-09-18 17:15:29 +0530

    Update provider.tf

--- Challenge-1/K8-Cluster-Creation/k8-cluster/provider.tf
+++ Challenge-1/K8-Cluster-Creation/k8-cluster/provider.tf
@@ -1,12 +1,17 @@
-provider "google" {
-  version     = "3.84.0"
-  credentials = file(var.serviceaccountjson)
-  project     = var.project_id
-  region      = var.region
+terraform {
+  required_version = ">= 1.0.4"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.84.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "=3.84.0"
+    }
+  }
 }
-
-provider "google-beta" {
-  version     = "3.84.0"
+provider "google" {
   credentials = file(var.serviceaccountjson)
   project     = var.project_id
   region      = var.region
